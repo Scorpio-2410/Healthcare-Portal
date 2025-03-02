c4f3c14f6f03b4b08cb71078323652c491028e72
Nabidul Islam <Nabidulislam90@gmail.com>
2025-03-02 19:20:29 +1100

    Fixing locked files issue

---
+++ healthcare-portal-backend/Models/Patient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Healthcare_Patient_Portal.Models;
+
+public partial class Patient
+{
+    public int PatientId { get; set; }
+
+    public int UserId { get; set; }
+
+    public string UserGender { get; set; } = null!;
+
+    public virtual ICollection<Appointment> Appointments { get; set; } = new List<Appointment>();
+
+    public virtual ICollection<MedicalRecord> MedicalRecords { get; set; } = new List<MedicalRecord>();
+
+    public virtual ICollection<Prescription> Prescriptions { get; set; } = new List<Prescription>();
+
+    public virtual User User { get; set; } = null!;
+}
